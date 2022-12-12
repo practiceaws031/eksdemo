2b26341b2010c10ca54b1a096031c7061c894e72
satheesh <satheesht676@gmail.com>
2022-12-12 16:02:20 +0530

    adding changes again125gd

--- scripts/install_agents.sh
+++ scripts/install_agents.sh
@@ -2,6 +2,8 @@
 echo "######################## installing aws cli #############################"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
+sudo ./aws/install
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin
 aws --version
 
 echo "######################## install kubectl ################################"
