9da002e66ea918002778be29d02065a34a8a0ed1
satheesh <satheesht676@gmail.com>
2022-12-12 17:09:13 +0530

    adding changes again125gd

--- scripts/install_agents.sh
+++ scripts/install_agents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "######################## installing aws cli #############################"
-sudo yum remove awscli -y
+sudo rm -rf /usr/local/aws
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --update
